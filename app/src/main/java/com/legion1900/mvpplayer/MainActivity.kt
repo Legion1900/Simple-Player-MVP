4c058c77d6807fb5867b72d75e84876348d238d0
Chumachenko Roman <rom4ik730@gmail.com>
2019-10-29 11:55:42 +0200

    Initial commit.

---
+++ app/src/main/java/com/legion1900/mvpplayer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.legion1900.mvpplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
