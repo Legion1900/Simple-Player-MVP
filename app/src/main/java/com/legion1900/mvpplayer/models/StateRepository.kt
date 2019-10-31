bc4f4b3178c732a9af927dbce27bf8a1b68db158
Chumachenko Roman <rom4ik730@gmail.com>
2019-10-31 17:05:32 +0200

    Implemented player save state operation logic.

---
+++ app/src/main/java/com/legion1900/mvpplayer/models/StateRepository.kt
@@ -0,0 +1,33 @@
+package com.legion1900.mvpplayer.models
+
+import android.content.SharedPreferences
+import com.legion1900.mvpplayer.contracts.PlayerContract
+
+class StateRepository(private val pref: SharedPreferences) : PlayerContract.Repository {
+
+    private companion object SharedPrefKeys {
+        const val SONG = "song"
+        const val MUSICIAN = "musician"
+        const val GENRE = "genre"
+        const val PATH = "path"
+    }
+
+    override fun loadLastSong(): PlayerContract.ModelSong? {
+        val song = pref.getString(SONG, null)
+        val musician = pref.getString(MUSICIAN, null)
+        val genre = pref.getString(GENRE, null)
+        val path = pref.getString(PATH, null)
+        return if (song == null || musician == null || genre == null || path == null) null
+        else Song(song, musician, genre, path)
+    }
+
+    override fun saveLastSong(song: PlayerContract.ModelSong) {
+        val editor = pref.edit()
+        editor.putString(SONG, song.name)
+        editor.putString(MUSICIAN, song.musician)
+        editor.putString(GENRE, song.genre)
+        editor.putString(PATH, song.path)
+        editor.apply()
+    }
+
+}
